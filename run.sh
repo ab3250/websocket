42e96b6f57fa236cdddeaa591a5e25351876b1b4
AB <ab3250@live.com>
2021-05-18 12:40:46 -0400

    base3

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-GUILE_LOAD_PATH="$PWD:$GUILE_LOAD_PATH" guile test.scm
+sudo $(pwd)/nginx/sbin/nginx -c $(pwd)/nginx/conf/nginx.conf &
+/usr/bin/google-chrome-stable localhost &
+guile start.scm
+
+
+
+#GUILE_LOAD_PATH="$PWD:$GUILE_LOAD_PATH" guile test.scm
 #/media/stor/gentoo-files/execs/compilers/programs/scheme/guile/SSE/upstream/guile-websocket#
-sudo lighttpd -D -f /etc/lighttpd/lighttpd.conf
+#sudo lighttpd -D -f /etc/lighttpd/lighttpd.conf
